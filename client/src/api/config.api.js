a3a65e9ca2c5e40e94179472b47d50e9d19a97e7
Joe <josephc.lambert@gmail.com>
2019-10-18 09:06:16 -0700

    sets up user login api

---
+++ client/src/api/config.api.js
@@ -0,0 +1,2 @@
+const baseURL = process.env.REACT_APP_PRODUCTION || `http://localhost:5000`;
+export const API_ROOT = `${baseURL}/api`;
---
+++ client/src/api/index.js
@@ -0,0 +1,3 @@
+export { API_ROOT } from './config.api';
+export { axiosWithAuth } from './axios.auth.api';
+export { login } from './login.api';
